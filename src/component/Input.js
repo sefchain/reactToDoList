ae682b6b7ba6353d3ed5b624158e8ae7b5876cd6
sefchain <119354272+sefchain@users.noreply.github.com>
2023-02-18 16:31:49 +0100

    Add files via upload

--- src/component/Input.js
+++ src/component/Input.js
@@ -6,6 +6,7 @@ function Input(props) {
         style={{ width: "200px", margin: "10px" }}
         value={props.Nom}
         onChange={(e) => props.SetNom(e.target.value)}
+        placeholder="Entrer un task"
       />
     </form>
   );
